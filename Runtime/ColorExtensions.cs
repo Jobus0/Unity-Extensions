808a872c1ec1a4be0a68a2107c710be9d61a145d
Johan <johan.tylon@gmail.com>
2022-04-15 11:28:19 +0200

    Add absolute version of ColorExtensions.WithAlpha()

--- Runtime/ColorExtensions.cs
+++ Runtime/ColorExtensions.cs
@@ -5,20 +5,40 @@ namespace Jobus.Extensions
     public static class ColorExtensions
     {
         /// <summary>
-        /// Returns a color with its alpha value multiplied.
+        /// Returns the color with a new alpha value.
+        /// </summary>
+        /// <param name="alpha">Range 0 to 1.</param>
+        public static Color WithAlpha(this Color color, float alpha)
+        {
+            color.a = alpha;
+            return color;
+        }
+        
+        /// <summary>
+        /// Returns the color with a new alpha value.
+        /// </summary>
+        /// <param name="alpha">Range 0 to 255.</param>
+        public static Color WithAlpha(this Color32 color, byte alpha)
+        {
+            color.a = alpha;
+            return color;
+        }
+
+		/// <summary>
+        /// Returns the color with its alpha value multiplied.
         /// </summary>
         /// <param name="alphaMultiplier">Range 0 to 1. Multiplies the current alpha, so using 0.5 for a color that already has 0.5 alpha gives 0.25.</param>
-        public static Color WithAlpha(this Color color, float alphaMultiplier)
+        public static Color WithAlphaRelative(this Color color, float alphaMultiplier)
         {
             color.a *= alphaMultiplier;
             return color;
         }
         
         /// <summary>
-        /// Returns a color with its alpha value multiplied.
+        /// Returns the color with its alpha value multiplied.
         /// </summary>
         /// <param name="alphaMultiplier">Range 0 to 1. Multiplies the current alpha, so using 0.5 for a color with 128 alpha gives 64.</param>
-        public static Color WithAlpha(this Color32 color, float alphaMultiplier)
+        public static Color WithAlphaRelative(this Color32 color, float alphaMultiplier)
         {
             color.a = (byte)(color.a*alphaMultiplier);
             return color;
